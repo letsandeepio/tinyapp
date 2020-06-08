ea8702a313aa288752452f93c8fdeabd68288648
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-06-08 23:11:25 +0000

    added new url function

--- express_server.js
+++ express_server.js
@@ -4,6 +4,21 @@ const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+const generateRandomString = (n) => {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUXYZ';
+  let random = '';
+  while (random.length < n) {
+    randomAlpha = alphabet[Math.floor(Math.random() * alphabet.length)];
+    random += Math.round(Math.random())
+      ? randomAlpha
+      : randomAlpha.toLowerCase();
+  }
+  return random;
+};
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -22,10 +37,20 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:url', (req, res) => {
   let templateVars = {
     longURL: urlDatabase[req.params.url],
     shortURL: req.params.url
   };
+
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
 });
